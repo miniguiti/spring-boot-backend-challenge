3c170c778439808e940e5f3fff31bb5c6dcee3f8
miniguiti <gminiguiti@outlook.com>
2021-07-21 22:39:38 -0300

    Controller advice

--- src/main/java/com/spring/challenge/restapijava/config/ErroDeFormularioDto.java
+++ src/main/java/com/spring/challenge/restapijava/config/ErroDeFormularioDto.java
@@ -1,4 +1,19 @@
 package com.spring.challenge.restapijava.config;
 
 public class ErroDeFormularioDto {
+    public String campo;
+    public String mensagem;
+
+    public ErroDeFormularioDto(String campo, String mensagem) {
+        this.campo = campo;
+        this.mensagem = mensagem;
+    }
+
+    public String getCampo() {
+        return campo;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
 }
